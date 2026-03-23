eda86df80b9a6e0fc05b227f7cfb99d1db51ae2c
Aarav Sethi <aarav.sethi@outlook.com>
2026-03-22 20:53:25 -0500

    Update extensions.go

--- compiler/extensions.go
+++ compiler/extensions.go
@@ -9,16 +9,22 @@ import (
 
 //go:embed templates/math.luau
 var math_extension string
+
 //go:embed templates/memory.luau
 var memory_extension string
+
 //go:embed templates/string.luau
 var string_extension string
+
 //go:embed templates/ctype.luau
 var ctype_extension string
+
 //go:embed templates/stdio.luau
 var stdio_extension string
+
 //go:embed templates/stdlib.luau
 var stdlib_extension string
+
 //go:embed templates/time.luau
 var time_extension string
 
@@ -51,7 +57,7 @@ func generateExtensions(writer *OutputWriter) string {
 
 	defaultLibs := []string{"memory", "string", "ctype", "stdlib", "stdio", "time"}
 	for _, name := range defaultLibs {
-		if ext, ok := extensions[name]; ok && !included[name] {
+		if ext, ok := extensions[name]; ok && included[name] {
 			sb.WriteString(ext)
 			sb.WriteString("\n")
 			included[name] = true
